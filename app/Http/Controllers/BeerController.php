0035aee29adccf85e23b5df8bd93dc898508a09d
marco bottoni <bottoni.mrc@gmail.com>
2021-02-10 15:48:35 +0100

    crud 1.0

---
+++ app/Http/Controllers/BeerController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Beer;
+
+class BeerController extends Controller
+{
+    public function index()
+    {
+        $beers = Beer::all();
+        dd($beers);
+        return view('beers');
+    }
+
+}
